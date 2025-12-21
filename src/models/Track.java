bebe3dff86fc598b47d91b78dc7cd039a8e1e242
Jayarani05 <jayaranimanimaran13@gmail.com>
2025-12-21 15:45:00 +0530

    Add Track model

---
+++ src/models/Track.java
@@ -0,0 +1,11 @@
+﻿package models;
+
+public class Track {
+    public String id;
+    public int lengthKm;
+    
+    public Track(String id, int lengthKm) {
+        this.id = id;
+        this.lengthKm = lengthKm;
+    }
+}
