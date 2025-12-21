8de800ea83e5f1fe6d48ed91a8c55f1caf2d8115
Jayarani05 <jayaranimanimaran13@gmail.com>
2025-12-21 10:00:00 +0530

    Create Train model class

---
+++ src/models/Train.java
@@ -0,0 +1,11 @@
+﻿package models;
+
+public class Train {
+    public String id;
+    public String name;
+    
+    public Train(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
