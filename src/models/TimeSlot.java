1b2984b922b65d468c98361faa61a646fffc556f
Jayarani05 <jayaranimanimaran13@gmail.com>
2025-12-22 09:30:00 +0530

    Implement TimeSlot class

---
+++ src/models/TimeSlot.java
@@ -0,0 +1,11 @@
+﻿package models;
+
+public class TimeSlot {
+    public long startTime;
+    public long endTime;
+    
+    public TimeSlot(long start, long end) {
+        this.startTime = start;
+        this.endTime = end;
+    }
+}
