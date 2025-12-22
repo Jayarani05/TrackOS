79511eb4c0208722c36c4bf9b4bde2033c61fee3
Jayarani05 <jayaranimanimaran13@gmail.com>
2025-12-22 16:00:00 +0530

    Add Station model

---
+++ src/models/Station.java
@@ -0,0 +1,13 @@
+﻿package models;
+
+public class Station {
+    public String id;
+    public String name;
+    public int platformCount;
+    
+    public Station(String id, String name, int platforms) {
+        this.id = id;
+        this.name = name;
+        this.platformCount = platforms;
+    }
+}
