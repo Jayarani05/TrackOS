003442ab812d7a40eb082f33f2657752192e8d7a
Jayarani05 <jayaranimanimaran13@gmail.com>
2025-12-23 11:00:00 +0530

    Create basic interval tree structure

---
+++ src/datastructures/IntervalTree.java
@@ -0,0 +1,16 @@
+﻿package datastructures;
+
+import models.TimeSlot;
+
+public class IntervalTree {
+    private Node root;
+    
+    private static class Node {
+        TimeSlot slot;
+        Node left, right;
+        
+        Node(TimeSlot slot) {
+            this.slot = slot;
+        }
+    }
+}
