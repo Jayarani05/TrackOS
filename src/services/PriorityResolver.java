2cb7fb3a819c9da5c2393a9263df10bc28bccf13
Jayarani05 <jayaranimanimaran13@gmail.com>
2025-12-26 13:30:00 +0530

    Add priority resolver logic

---
+++ src/services/PriorityResolver.java
@@ -0,0 +1,11 @@
+﻿package services;
+
+import models.Train;
+
+public class PriorityResolver {
+    
+    public Train resolve(Train a, Train b) {
+        // Higher priority wins
+        return a;  // TODO: implement comparison
+    }
+}
