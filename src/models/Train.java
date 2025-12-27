8914283a0cc3405c2f768a4208e6930315fc4d96
Jayarani05 <jayaranimanimaran13@gmail.com>
2025-12-27 10:45:00 +0530

    Implement train priority scoring

--- src/models/Train.java
+++ src/models/Train.java
@@ -3,9 +3,24 @@
 public class Train {
     public String id;
     public String name;
+    public TrainType type;
+    public int passengerCount;
+    public int delayMinutes;
     
-    public Train(String id, String name) {
+    public Train(String id, String name, TrainType type) {
         this.id = id;
         this.name = name;
+        this.type = type;
+    }
+    
+    public int getPriority() {
+        int score = 0;
+        if (type == TrainType.EXPRESS) score += 100;
+        else if (type == TrainType.LOCAL) score += 50;
+        else score += 25;
+        
+        score += passengerCount / 10;
+        score += delayMinutes * 2;
+        return score;
     }
 }
---
+++ src/models/TrainType.java
@@ -0,0 +1,7 @@
+﻿package models;
+
+public enum TrainType {
+    EXPRESS,
+    LOCAL,
+    CARGO
+}
