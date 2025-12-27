74dd18566a51f62b84c38e32177618d1229f7387
Jayarani05 <jayaranimanimaran13@gmail.com>
2025-12-27 16:15:00 +0530

    Add graph structure for rail network

---
+++ src/datastructures/RailNetwork.java
@@ -0,0 +1,17 @@
+﻿package datastructures;
+
+import models.Station;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
+public class RailNetwork {
+    private Map<String, Station> stations = new HashMap<>();
+    private Map<String, List<String>> connections = new HashMap<>();
+    
+    public void addStation(Station s) {
+        stations.put(s.id, s);
+        connections.put(s.id, new ArrayList<>());
+    }
+}
