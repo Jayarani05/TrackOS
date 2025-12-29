e3bc11df12a6e08c597f433cfed2d95cc3e33f3a
Jayarani05 <jayaranimanimaran13@gmail.com>
2025-12-29 09:30:00 +0530

    Create slot allocator service

---
+++ src/services/SlotAllocator.java
@@ -0,0 +1,19 @@
+﻿package services;
+
+import models.TimeSlot;
+
+public class SlotAllocator {
+    private ConflictDetector detector;
+    
+    public SlotAllocator(ConflictDetector detector) {
+        this.detector = detector;
+    }
+    
+    public boolean allocate(TimeSlot slot) {
+        if (detector.hasConflict(slot)) {
+            return false;
+        }
+        detector.register(slot);
+        return true;
+    }
+}
