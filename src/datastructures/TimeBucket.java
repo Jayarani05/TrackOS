9ffae353ce8087d789980cca4fde931be6a2bb4c
Jayarani05 <jayaranimanimaran13@gmail.com>
2025-12-30 16:30:00 +0530

    Add time bucket for capacity

---
+++ src/datastructures/TimeBucket.java
@@ -0,0 +1,21 @@
+﻿package datastructures;
+
+public class TimeBucket {
+    public long startTime;
+    public long endTime;
+    public int count;
+    
+    public TimeBucket(long start, long end) {
+        this.startTime = start;
+        this.endTime = end;
+        this.count = 0;
+    }
+    
+    public void increment() {
+        count++;
+    }
+    
+    public boolean contains(long time) {
+        return time >= startTime && time < endTime;
+    }
+}
