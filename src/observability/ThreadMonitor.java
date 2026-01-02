fd758694bf073d8e86dd946c3891ac15ad25b20e
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-02 09:45:00 +0530

    Implement JVM thread monitor

---
+++ src/observability/ThreadMonitor.java
@@ -0,0 +1,20 @@
+﻿package observability;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+
+public class ThreadMonitor {
+    private ThreadMXBean bean;
+    
+    public ThreadMonitor() {
+        this.bean = ManagementFactory.getThreadMXBean();
+    }
+    
+    public int getThreadCount() {
+        return bean.getThreadCount();
+    }
+    
+    public int getPeakCount() {
+        return bean.getPeakThreadCount();
+    }
+}
