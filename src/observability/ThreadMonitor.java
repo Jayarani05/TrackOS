faa694cb0f377038c0fa7cdb2582c2c5d77f9ea4
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-02 14:30:00 +0530

    Add thread state tracking

--- src/observability/ThreadMonitor.java
+++ src/observability/ThreadMonitor.java
@@ -2,6 +2,7 @@
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
+import java.lang.management.ThreadInfo;
 
 public class ThreadMonitor {
     private ThreadMXBean bean;
@@ -16,5 +17,26 @@ public class ThreadMonitor {
     
     public int getPeakCount() {
         return bean.getPeakThreadCount();
+    }
+    
+    public ThreadStats getStats() {
+        ThreadStats s = new ThreadStats();
+        s.total = bean.getThreadCount();
+        
+        ThreadInfo[] infos = bean.getThreadInfo(bean.getAllThreadIds());
+        for (ThreadInfo i : infos) {
+            if (i == null) continue;
+            switch (i.getThreadState()) {
+                case RUNNABLE: s.runnable++; break;
+                case BLOCKED: s.blocked++; break;
+                case WAITING:
+                case TIMED_WAITING: s.waiting++; break;
+            }
+        }
+        return s;
+    }
+    
+    public static class ThreadStats {
+        public int total, runnable, blocked, waiting;
     }
 }
