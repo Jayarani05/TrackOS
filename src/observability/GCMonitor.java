1861b0d3236cd3dc552490cce6b107e16f0d883a
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-03 10:30:00 +0530

    Create GC pause detector

---
+++ src/observability/GCMonitor.java
@@ -0,0 +1,33 @@
+﻿package observability;
+
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
+public class GCMonitor {
+    private List<GarbageCollectorMXBean> beans;
+    
+    public GCMonitor() {
+        this.beans = ManagementFactory.getGarbageCollectorMXBeans();
+    }
+    
+    public long getTotalCollections() {
+        long total = 0;
+        for (GarbageCollectorMXBean b : beans) {
+            if (b.getCollectionCount() > 0) {
+                total += b.getCollectionCount();
+            }
+        }
+        return total;
+    }
+    
+    public long getTotalTimeMs() {
+        long total = 0;
+        for (GarbageCollectorMXBean b : beans) {
+            if (b.getCollectionTime() > 0) {
+                total += b.getCollectionTime();
+            }
+        }
+        return total;
+    }
+}
