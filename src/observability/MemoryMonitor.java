350f4bd6332ecf40f39180c146fd203492f6e119
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-03 16:00:00 +0530

    Add memory pressure monitor

---
+++ src/observability/MemoryMonitor.java
@@ -0,0 +1,30 @@
+﻿package observability;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+
+public class MemoryMonitor {
+    private MemoryMXBean bean;
+    
+    public MemoryMonitor() {
+        this.bean = ManagementFactory.getMemoryMXBean();
+    }
+    
+    public long getHeapUsedMB() {
+        return bean.getHeapMemoryUsage().getUsed() / (1024*1024);
+    }
+    
+    public long getHeapMaxMB() {
+        return bean.getHeapMemoryUsage().getMax() / (1024*1024);
+    }
+    
+    public double getHeapPercent() {
+        MemoryUsage h = bean.getHeapMemoryUsage();
+        return (h.getUsed() * 100.0) / h.getMax();
+    }
+    
+    public boolean hasPressure() {
+        return getHeapPercent() > 80;
+    }
+}
