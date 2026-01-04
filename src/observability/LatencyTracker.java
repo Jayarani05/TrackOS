a27bff389e5a23e3503edf239fd7131cec358b50
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-04 09:15:00 +0530

    Implement latency tracker

---
+++ src/observability/LatencyTracker.java
@@ -0,0 +1,21 @@
+﻿package observability;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LatencyTracker {
+    private List<Long> samples = new ArrayList<>();
+    private long threshold;
+    
+    public LatencyTracker(long thresholdMs) {
+        this.threshold = thresholdMs;
+    }
+    
+    public void record(long ms) {
+        samples.add(ms);
+    }
+    
+    public int getCount() {
+        return samples.size();
+    }
+}
