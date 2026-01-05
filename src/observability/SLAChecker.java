cd66bf9d31672081aceae894a8f646dc71076894
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-05 15:30:00 +0530

    Add SLA violation checker

---
+++ src/observability/SLAChecker.java
@@ -0,0 +1,19 @@
+﻿package observability;
+
+public class SLAChecker {
+    private SLAConfig config;
+    private LatencyTracker tracker;
+    
+    public SLAChecker(SLAConfig config, LatencyTracker tracker) {
+        this.config = config;
+        this.tracker = tracker;
+    }
+    
+    public boolean isMeetingSLA() {
+        return tracker.getSLACompliance() >= config.minCompliance;
+    }
+    
+    public boolean isLatencyOk(long ms) {
+        return ms <= config.maxLatencyMs;
+    }
+}
