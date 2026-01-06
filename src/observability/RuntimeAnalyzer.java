8d70b515e266c0a0f6b6e88bb68aa06323843a90
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-06 16:45:00 +0530

    Add runtime stats aggregator

---
+++ src/observability/RuntimeAnalyzer.java
@@ -0,0 +1,23 @@
+﻿package observability;
+
+public class RuntimeAnalyzer {
+    private ThreadMonitor threads;
+    private GCMonitor gc;
+    private MemoryMonitor memory;
+    private LatencyTracker latency;
+    
+    public RuntimeAnalyzer(long slaMs) {
+        this.threads = new ThreadMonitor();
+        this.gc = new GCMonitor();
+        this.memory = new MemoryMonitor();
+        this.latency = new LatencyTracker(slaMs);
+    }
+    
+    public void recordLatency(long ms) {
+        latency.record(ms);
+    }
+    
+    public boolean isHealthy() {
+        return !memory.hasPressure() && latency.getSLACompliance() >= 99.0;
+    }
+}
