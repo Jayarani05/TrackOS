16e689f5c196beb0348165ffb65a2785b80d1ad8
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-07 09:30:00 +0530

    Create scheduling engine core

---
+++ src/services/SchedulingEngine.java
@@ -0,0 +1,15 @@
+﻿package services;
+
+import datastructures.RailNetwork;
+
+public class SchedulingEngine {
+    private ConflictDetector detector;
+    private PriorityResolver resolver;
+    private RailNetwork network;
+    
+    public SchedulingEngine(RailNetwork network) {
+        this.network = network;
+        this.detector = new ConflictDetector();
+        this.resolver = new PriorityResolver();
+    }
+}
