7273c59e8960b03cc8cb78873a50a00f808f57e1
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-08 10:45:00 +0530

    Connect priority resolver

--- src/models/TimeSlot.java
+++ src/models/TimeSlot.java
@@ -3,9 +3,17 @@
 public class TimeSlot {
     public long startTime;
     public long endTime;
+    public Train train;
+    public Track track;
     
-    public TimeSlot(long start, long end) {
+    public TimeSlot(long start, long end, Train train, Track track) {
         this.startTime = start;
         this.endTime = end;
+        this.train = train;
+        this.track = track;
+    }
+    
+    public boolean overlaps(TimeSlot other) {
+        return startTime < other.endTime && other.startTime < endTime;
     }
 }
--- src/services/PriorityResolver.java
+++ src/services/PriorityResolver.java
@@ -1,11 +1,20 @@
 ﻿package services;
 
 import models.Train;
+import models.TimeSlot;
+import java.util.List;
 
 public class PriorityResolver {
     
     public Train resolve(Train a, Train b) {
-        // Higher priority wins
-        return a;  // TODO: implement comparison
+        return a.getPriority() >= b.getPriority() ? a : b;
+    }
+    
+    public Train resolveConflict(TimeSlot newSlot, List<TimeSlot> conflicts) {
+        Train winner = newSlot.train;
+        for (TimeSlot c : conflicts) {
+            winner = resolve(winner, c.train);
+        }
+        return winner;
     }
 }
