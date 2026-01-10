076e381996bd8e0cafadbc5f4eb5920b7e48097a
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-10 16:00:00 +0530

    Add train arrival events

--- src/events/TrainEvent.java
+++ src/events/TrainEvent.java
@@ -1,15 +1,26 @@
 ﻿package events;
 
 import models.Train;
+import models.Station;
 
 public class TrainEvent {
     public String type;
     public Train train;
+    public Station station;
     public long timestamp;
     
-    public TrainEvent(String type, Train train) {
+    public TrainEvent(String type, Train train, Station station) {
         this.type = type;
         this.train = train;
+        this.station = station;
         this.timestamp = System.currentTimeMillis();
+    }
+    
+    public static TrainEvent arrival(Train t, Station s) {
+        return new TrainEvent("ARRIVAL", t, s);
+    }
+    
+    public static TrainEvent departure(Train t, Station s) {
+        return new TrainEvent("DEPARTURE", t, s);
     }
 }
