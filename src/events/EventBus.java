1cad8b1d0fb90cb27f494bd5dbf59a5324ce9267
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-10 10:15:00 +0530

    Create simple event bus

---
+++ src/events/EventBus.java
@@ -0,0 +1,19 @@
+﻿package events;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class EventBus {
+    private List<Consumer<TrainEvent>> listeners = new ArrayList<>();
+    
+    public void subscribe(Consumer<TrainEvent> listener) {
+        listeners.add(listener);
+    }
+    
+    public void publish(TrainEvent event) {
+        for (Consumer<TrainEvent> l : listeners) {
+            l.accept(event);
+        }
+    }
+}
---
+++ src/events/TrainEvent.java
@@ -0,0 +1,15 @@
+﻿package events;
+
+import models.Train;
+
+public class TrainEvent {
+    public String type;
+    public Train train;
+    public long timestamp;
+    
+    public TrainEvent(String type, Train train) {
+        this.type = type;
+        this.train = train;
+        this.timestamp = System.currentTimeMillis();
+    }
+}
