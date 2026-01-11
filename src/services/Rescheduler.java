2f9d4554e45064e807795c9ef215be8fbd7f3e9b
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-11 15:45:00 +0530

    Add rescheduling support

---
+++ src/services/Rescheduler.java
@@ -0,0 +1,16 @@
+﻿package services;
+
+import models.*;
+
+public class Rescheduler {
+    private SchedulingEngine engine;
+    
+    public Rescheduler(SchedulingEngine engine) {
+        this.engine = engine;
+    }
+    
+    public SchedulingResult reschedule(TimeSlot oldSlot, long newStart, long newEnd) {
+        TimeSlot newSlot = new TimeSlot(newStart, newEnd, oldSlot.train, oldSlot.track);
+        return engine.schedule(newSlot);
+    }
+}
