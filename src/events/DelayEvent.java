906fb6ffad654b89d3b5c02e24418e4ec280db3e
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-11 11:30:00 +0530

    Implement delay handling

---
+++ src/events/DelayEvent.java
@@ -0,0 +1,17 @@
+﻿package events;
+
+import models.Train;
+
+public class DelayEvent {
+    public Train train;
+    public int delayMinutes;
+    public String reason;
+    public long timestamp;
+    
+    public DelayEvent(Train train, int minutes, String reason) {
+        this.train = train;
+        this.delayMinutes = minutes;
+        this.reason = reason;
+        this.timestamp = System.currentTimeMillis();
+    }
+}
---
+++ src/services/DelayHandler.java
@@ -0,0 +1,13 @@
+﻿package services;
+
+import events.DelayEvent;
+import models.Train;
+
+public class DelayHandler {
+    
+    public void handleDelay(DelayEvent event) {
+        Train t = event.train;
+        t.delayMinutes += event.delayMinutes;
+        System.out.println(t.name + " delayed by " + event.delayMinutes + " min");
+    }
+}
