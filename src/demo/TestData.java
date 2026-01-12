2e7e19b5dd695f6b5db955e5263443c37e2592e2
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-12 09:45:00 +0530

    Create test train data

---
+++ src/demo/TestData.java
@@ -0,0 +1,22 @@
+﻿package demo;
+
+import models.*;
+
+public class TestData {
+    
+    public static Train expressA() {
+        Train t = new Train("T1", "Express 101", TrainType.EXPRESS);
+        t.passengerCount = 500;
+        return t;
+    }
+    
+    public static Train localA() {
+        Train t = new Train("T2", "Local 201", TrainType.LOCAL);
+        t.passengerCount = 200;
+        return t;
+    }
+    
+    public static Train cargoA() {
+        return new Train("T3", "Cargo 301", TrainType.CARGO);
+    }
+}
