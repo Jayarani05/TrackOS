95f64fc7f2d9763dde60459d962a05da3b0cc818
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-13 15:15:00 +0530

    Add console output formatting

---
+++ src/util/Printer.java
@@ -0,0 +1,20 @@
+﻿package util;
+
+public class Printer {
+    
+    public static void header(String text) {
+        System.out.println();
+        System.out.println("=".repeat(40));
+        System.out.println("  " + text);
+        System.out.println("=".repeat(40));
+    }
+    
+    public static void section(String text) {
+        System.out.println();
+        System.out.println("--- " + text + " ---");
+    }
+    
+    public static void item(String text) {
+        System.out.println("  * " + text);
+    }
+}
