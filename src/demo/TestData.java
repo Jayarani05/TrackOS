40a5df2afaf76cdb0d00598d817f7efdb92089fc
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-14 11:30:00 +0530

    Add more test scenarios

--- src/demo/TestData.java
+++ src/demo/TestData.java
@@ -10,13 +10,30 @@ public class TestData {
         return t;
     }
     
+    public static Train expressB() {
+        Train t = new Train("T4", "Express 102", TrainType.EXPRESS);
+        t.passengerCount = 450;
+        t.delayMinutes = 10;
+        return t;
+    }
+    
     public static Train localA() {
         Train t = new Train("T2", "Local 201", TrainType.LOCAL);
         t.passengerCount = 200;
         return t;
     }
     
+    public static Train localB() {
+        Train t = new Train("T5", "Local 202", TrainType.LOCAL);
+        t.passengerCount = 150;
+        return t;
+    }
+    
     public static Train cargoA() {
         return new Train("T3", "Cargo 301", TrainType.CARGO);
+    }
+    
+    public static Train cargoB() {
+        return new Train("T6", "Cargo 302", TrainType.CARGO);
     }
 }
