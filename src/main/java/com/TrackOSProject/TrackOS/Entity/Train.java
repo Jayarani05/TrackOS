62daf9dccbcda5b8c99dc2d3d93eabce4e677ca0
Jayarani05 <jayaranimanimaran13@gmail.com>
2026-01-19 14:45:00 +0530

    feat: add Train entity with JPA annotations

---
+++ src/main/java/com/TrackOSProject/TrackOS/Entity/Train.java
@@ -0,0 +1,58 @@
+package com.TrackOSProject.TrackOS.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "trains")
+public class Train {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+    private int capacity;
+
+    public Train() {
+    }
+
+    public Train(String name, String type, int capacity) {
+        this.name = name;
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
